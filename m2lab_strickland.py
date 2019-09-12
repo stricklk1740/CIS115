c0930ac2d91aecac7526b3f24d491cf8e1ea794c
stricklk1740 <55244430+stricklk1740@users.noreply.github.com>
2019-09-12 10:43:44 -0400

    Yup

---
+++ m2lab_strickland.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+M2Lab
+Strickland
+9/12/19
+"""
+
+#convert M2T1 to Python
+
+#//M2Lab
+#//Kenny Strickland
+#//9/12/19
+#
+#
+#//Greet user by name
+#//tell user their age on next birthday
+#
+#
+#//declaration step
+#Declare string name
+#Declare integer age
+#
+#
+#//Ask for name
+#display "What is your name?"
+#input (name) 
+#display "Greetings (name)"
+
+print("What is your name?")
+name = input()
+print ("Greetings" name) 
+#
+#//Ask for their age
+#display "What is your age?"
+#input_integer (age)
+print("What is your age?")
+age= int((input))
+
+#
+#//tell user by name their age on next birthday
+#nextAge= (age+1)
+#display "Well (name) on your next birthday you will turn (nextAge)"
+
+nextAge= int((age+1))
+print ("Well", name "On Your birthday you will be" nextAge)
